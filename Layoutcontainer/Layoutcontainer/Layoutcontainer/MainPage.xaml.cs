09b932c101df9436bc24b55a290ec5b823897173
Michael Zöhling <michael.zoehling@outlook.com>
2019-03-26 17:02:55 +0100

    LayoutContainer

--- Layoutcontainer/Layoutcontainer/Layoutcontainer/MainPage.xaml.cs
+++ Layoutcontainer/Layoutcontainer/Layoutcontainer/MainPage.xaml.cs
@@ -13,5 +13,23 @@ namespace Layoutcontainer
         {
             InitializeComponent();
         }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            //var button = sender as Button;
+            ////App.Current.Resources["MeinStyle"]
+            //button.Style = (Style)Resources["NewStyle"];
+            //gridRoot.Resources["specialButtonStyle"] = Resources["NewStyle"];
+
+            // Wenn die Styles in der ContentPage sind:
+            // Resources["currentLabelStyle"]= Resources["labelDarkStyle"]
+
+            // Wenn die Styles in der App.Xaml sind:
+            // App.Current.Resources["currentLabelStyle"]= App.Current.Resources["labelDarkStyle"]
+
+            // Styles direkt setzen:
+            // labelÜberschrift.Style = (Style)Resources["labelDarkStyle"]
+            // labelUnterüberschrift.Style = (Style)Resources["labelDarkStyle"]
+        }
     }
 }
