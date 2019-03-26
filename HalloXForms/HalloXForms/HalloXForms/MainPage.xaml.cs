1ca1784e05b67b2f741c54f77cb0ef3ba437f377
Michael Zöhling <michael.zoehling@outlook.com>
2019-03-26 12:03:27 +0100

    DisplayAlert und DisplayActionSheet

--- HalloXForms/HalloXForms/HalloXForms/MainPage.xaml.cs
+++ HalloXForms/HalloXForms/HalloXForms/MainPage.xaml.cs
@@ -13,5 +13,20 @@ namespace HalloXForms
         {
             InitializeComponent();
         }
+
+        private async void ButtonKlickMich_Clicked(object sender, EventArgs e)
+        {
+            // Messagebox mit 1 Antwort
+            // DisplayAlert("Nachricht", "Du hast den Button geklickt", "OK");
+
+            // Messagebox mit 2 Antworten ("Ja/Nein")
+            // var result = await DisplayAlert("Frage", "Mittagspause?", "Ja", "Nein");
+            // await DisplayAlert("Antwort", $"Deine Antwort war: {result}", "Ok");
+
+            // MessageBox mit mehreren Möglichkeiten
+            string result = await DisplayActionSheet("Wähle deine Lieblingsfarbe", null, null, "Rot", "Gelb", "Grün", "Orange", "Blau");
+            await DisplayAlert("Antwort", $"Deine Antwort war: {result}", "Ok");
+
+        }
     }
 }
