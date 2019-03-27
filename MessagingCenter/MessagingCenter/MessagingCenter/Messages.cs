d06d93242aa448c504798d163526a20720d10098
Michael Zöhling <michael.zoehling@outlook.com>
2019-03-27 16:43:37 +0100

    MessagingCenter

---
+++ MessagingCenter/MessagingCenter/MessagingCenter/Messages.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MessagingCenter
+{
+    public static class MSG
+    {
+        public static string SliderValueChanged = "SVC";
+    }
+}
---
+++ MessagingCenter/MessagingCenter/MessagingCenter/Seite 2.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MessagingCenter
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Seite2 : ContentPage
+	{
+		public Seite2 ()
+		{
+			InitializeComponent ();
+		}
+
+        private void Slider_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            var slider = sender as Slider;
+            Xamarin.Forms.MessagingCenter.Send(slider, MSG.SliderValueChanged, e.NewValue);
+        }
+    }
+}
