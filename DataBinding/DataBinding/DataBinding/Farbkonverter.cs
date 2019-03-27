ca247d1b0db51d9bf70f0147561ea8bc96c6573f
Michael Zöhling <michael.zoehling@outlook.com>
2019-03-27 09:52:02 +0100

    Databinding und ValueConverter

---
+++ DataBinding/DataBinding/DataBinding/Farbkonverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using Xamarin.Forms;
+
+namespace DataBinding
+{
+    class Farbkonverter : IValueConverter
+    {
+        // OneWay
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string color = value as string;
+            if (color == null)
+                return Color.White;
+
+            var resultColor = typeof(Color).GetFields().FirstOrDefault(x => x.Name.ToLower() == color.ToLower());
+            if (resultColor == null)
+                return Color.White;
+            else
+                return resultColor.GetValue(typeof(Color));
+        }
+
+        // Wichtig beim TwoWayBinding
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
