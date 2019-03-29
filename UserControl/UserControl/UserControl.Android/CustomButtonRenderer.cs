c37a6fd51f3e958ea118c28b63ee26c2f513db76
Michael Zöhling <michael.zoehling@outlook.com>
2019-03-29 13:31:10 +0100

    CustomRenderer für Android-Button

---
+++ UserControl/UserControl/UserControl.Android/CustomButtonRenderer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using UserControl;
+using UserControl.Droid;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(CustomButton),typeof(CustomButtonRenderer))]
+namespace UserControl.Droid
+{
+    class CustomButtonRenderer : ButtonRenderer
+    {
+        public CustomButtonRenderer(Context context) : base(context)
+        {
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Xamarin.Forms.Button> e)
+        {
+            base.OnElementChanged(e);
+
+            // Control.SetBackgroundColor(Android.Graphics.Color.LightGreen);
+            Control.SetBackgroundResource(Resource.Drawable.gradientBrush);
+        }
+    }
+}
