79f277907d1fb88150bbd36075ca1e3ebc050077
Michael Zöhling <michael.zoehling@outlook.com>
2019-03-29 13:17:36 +0100

    CustomButton in UWP

---
+++ UserControl/UserControl/UserControl.UWP/CustomButtonRenderer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UserControl;
+using UserControl.UWP;
+using Windows.UI.Xaml.Media;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.UWP;
+
+[assembly: ExportRenderer(typeof(CustomButton),typeof(CustomButtonRenderer))]
+namespace UserControl.UWP
+{
+    class CustomButtonRenderer : ButtonRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Button> e)
+        {
+            base.OnElementChanged(e);
+
+            Control.Background = new LinearGradientBrush
+            {
+                GradientStops = new GradientStopCollection
+                {
+                    new GradientStop{Offset=0,Color=Windows.UI.Colors.DarkTurquoise},
+                    new GradientStop{Offset=0.5,Color=Windows.UI.Colors.LightCoral},
+                    new GradientStop{Offset=1,Color=Windows.UI.Colors.SlateBlue},
+                }
+            };
+        }
+    }
+}
---
+++ UserControl/UserControl/UserControl/EmailEntryBehavior.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+using Xamarin.Forms;
+
+namespace UserControl
+{
+    public class EmailEntryBehavior : Behavior<Entry>
+    {
+        protected override void OnAttachedTo(Entry bindable)
+        {
+            base.OnAttachedTo(bindable);
+            bindable.TextChanged += Bindable_TextChanged;
+        }
+        protected override void OnDetachingFrom(Entry bindable)
+        {
+            base.OnDetachingFrom(bindable);
+            bindable.TextChanged -= Bindable_TextChanged;
+        }
+
+        private void Bindable_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if(Regex.IsMatch(e.NewTextValue, @"[a-zA-Z0-9\.]+@[a-zA-Z0-9.]+\.[a-zA-Z]{2,5}"))
+            {
+                (sender as Entry).TextColor = Color.Black;
+            }
+            else
+            {
+                (sender as Entry).TextColor = Color.Red;
+            }
+        }
+    }
+}
