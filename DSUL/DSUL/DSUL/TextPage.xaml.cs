09dad2a7a26c71e9944923bb3ac0d171803241df
Michael Zöhling <michael.zoehling@outlook.com>
2019-03-29 10:55:47 +0100

    Variante mit Xamarin.Essentials

--- DSUL/DSUL/DSUL/TextPage.xaml.cs
+++ DSUL/DSUL/DSUL/TextPage.xaml.cs
@@ -1,9 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Xamarin.Essentials;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -31,6 +32,23 @@ namespace DSUL
             var service = DependencyService.Get<ITextFileHelper>();
 
             DisplayAlert("Textdokument",service.LoadTextFile("demo.txt"),"Yay");
+        }
+
+        private void ButtonEssentialsSpeichern_Clicked(object sender, EventArgs e)
+        {
+            //if(Device.RuntimePlatform == Device.WPF)
+            //{
+
+            //}
+            string fullPath = Path.Combine(FileSystem.CacheDirectory, "essentialsTest.txt");
+            File.WriteAllText(fullPath, entryEingabe.Text);
+            DisplayAlert("Essentials", "Datei wurde erfolgreich gespeichert", "OK");
+        }
+
+        private void ButtonEssentialsLaden_Clicked(object sender, EventArgs e)
+        {
+            string fullPath = Path.Combine(FileSystem.CacheDirectory, "essentialsTest.txt");
+            DisplayAlert("Essentials", File.ReadAllText(fullPath), "Yay");
         }
     }
 }
