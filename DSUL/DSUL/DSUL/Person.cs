0b6a0efa4823a81450822dcc111604b241ffbf76
Michael Zöhling <michael.zoehling@outlook.com>
2019-03-29 11:42:53 +0100

    SQLite

---
+++ DSUL/DSUL/DSUL/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using SQLite;
+
+namespace DSUL
+{
+    class Person
+    {
+        [PrimaryKey,AutoIncrement]
+        public int ID { get; set; }
+        [MaxLength(255)]
+        public string Vorname { get; set; }
+        [MaxLength(12)]
+        public string Nachname { get; set; }
+        public byte Alter { get; set; }
+        public decimal Kontostand { get; set; }
+    }
+}
---
+++ DSUL/DSUL/DSUL/SQLitePage.xaml.cs
@@ -0,0 +1,58 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace DSUL
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class SQLitePage : ContentPage
+	{
+		public SQLitePage ()
+		{
+			InitializeComponent ();
+
+            // Init:
+            string fullPath = string.Empty;
+            if (Device.RuntimePlatform == Device.WPF)
+                fullPath = "db.sqlite";
+            else
+                fullPath = Path.Combine(FileSystem.CacheDirectory, "db.sqlite");
+
+            con = new SQLiteAsyncConnection(fullPath); // Erstellt wenns nicht vorhanden ist oder öffnet es wenn es bereits existiert
+            // Nur beim ersten starten relevant:
+            con.CreateTableAsync<Person>(); // Wenn die Tabelle bereits existiert, passiert nichts
+		}
+        SQLiteAsyncConnection con;
+
+
+        private async void ButtonEinfügen_Clicked(object sender, EventArgs e)
+        {
+            Person p = new Person();
+            p.Vorname = entryName.Text;
+            p.Nachname = "Mustermann";
+            p.Alter = 10;
+            p.Kontostand = 100;
+
+            await con.InsertAsync(p);
+            await DisplayAlert("Message", "Person wurde erfolgreich gespeichert !", "OK");
+        }
+
+        private async void ListViewPersonen_Refreshing(object sender, EventArgs e)
+        {
+            listViewPersonen.ItemsSource = await con.Table<Person>().ToListAsync();
+            listViewPersonen.EndRefresh();
+        }
+
+        private async void ButtonRefreshWPF_Clicked(object sender, EventArgs e)
+        {
+            listViewPersonen.ItemsSource = await con.Table<Person>().ToListAsync();
+        }
+    }
+}
